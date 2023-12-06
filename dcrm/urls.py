22fa5f0968a06c1830a27f6c6475c5742cdb81b4
danielhou13 <danielhou13@gmail.com>
2023-12-05 20:37:58 -0800

    Add records migration and toolbar

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -17,4 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("website.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("website.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
+]
